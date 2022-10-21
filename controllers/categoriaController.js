903b01df71ff54879ca1a87b8830859cf4c5641b
jordi <jordi.mallafre@hotmail.com>
2022-10-21 10:45:54 +0200

    update, delete categoria

--- controllers/categoriaController.js
+++ controllers/categoriaController.js
@@ -14,6 +14,81 @@ class CategoriaController {
         }
     }
 
+    static async create_get(req, res, next) {
+        res.render('categories/new')
+    }
+
+    static create_post(req, res, next) {
+
+        Categoria.create(req.body, (error, newRecord) => {
+            if (error) {
+                res.render('categories/new', { error: 'error' })
+            } else {
+
+                res.redirect('/categories')
+            }
+        })
+    }
+
+    static update_get(req, res, next) {
+        Categoria.findById(req.params.id, function (err, list_categoria) {
+            if (err) {
+                return next(err);
+            }
+            if (list_categoria == null) {
+                // No results.
+                var err = new Error("Categoria not found");
+                err.status = 404;
+                return next(err);
+            }
+            // Success.
+            res.render("categories/update", { list: list_categoria });
+        });
+
+    }
+
+    static update_post(req, res, next) {
+        var list_categoria = {
+            name: req.body.name,
+            codi: req.body.codi,
+            _id: req.params.id,
+        };
+
+        Categoria.findByIdAndUpdate(
+            req.params.id,
+            list_categoria,
+            {},
+            function (err, thecategories) {
+                if (err) {
+
+                    res.render("categories/update", { list: list_categoria, error: err.message });
+
+                }
+
+                res.render("categories/update", { list: list_categoria, message: 'Categoria Updated' });
+                //res.redirect("/categories");
+
+            }
+        );
+    }
+
+    static async delete_get(req, res, next) {
+
+        res.render('categories/delete', { id: req.params.id })
+        
+    }
+
+    static async delete_post(req, res, next) {
+
+        Categoria.findByIdAndRemove(req.params.id, (error) => {
+            if (error) {
+                res.redirect('/categories')
+            } else {
+                res.redirect('/categories')
+            }
+        })
+    }
+
 }
 
 module.exports = CategoriaController;
--- routes/categoriesRouter.js
+++ routes/categoriesRouter.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 var categoria_controller = require('../controllers/categoriaController');
 
-router.get('/', categoria_controller.list);
+router.get('/', categoria_controller.list); 
+
+router.get('/create', categoria_controller.create_get); 
+router.post('/create', categoria_controller.create_post); 
+
+router.get("/delete/:id", categoria_controller.delete_get);
+router.post("/delete/:id", categoria_controller.delete_post);
+
+router.get("/update/:id", categoria_controller.update_get);
+router.post("/update/:id", categoria_controller.update_post);
 
 
 module.exports = router;
