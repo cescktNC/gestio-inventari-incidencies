626c5ae1d9918bc79b01446d686cac0fa93e154e
Francesc Navarro <cesccat82@gmail.com>
2022-11-09 10:44:56 +0100

    francesc

--- controllers/usuarisController.js
+++ controllers/usuarisController.js
@@ -1,13 +1,12 @@
 const bcrypt = require('bcrypt');
-const usuari = require('../models/usuari');
-var Usuari = require("../models/usuari");
+const Usuari = require("../models/usuari");
 
 class UsuariController {
 
     static async list(req,res,next) {
         try {
             var list_usuaris = await Usuari.find();
-            res.render('usuaris/list', {usuaris:list_usuaris});
+            res.render('usuaris/list', { usuaris:list_usuaris });
         }
         catch(e) {
             res.send('Error!');
@@ -16,7 +15,7 @@ class UsuariController {
 
     static create_get(req, res, next) {
         var list_carrecs = Usuari.schema.path('carrec').enumValues;
-        res.render('usuaris/new', {carrecs:list_carrecs});
+        res.render('usuaris/new', { carrecs:list_carrecs });
     }
 
     static async create_post(req, res) {
@@ -34,14 +33,75 @@ class UsuariController {
                 // Guardar usuari a la base de dades
                 Usuari.create(req.body, function (error, newUsuari) {
                     if(error) {
-                        res.render('/usuaris/new',{error:error.message});
+                        res.render('/usuaris/new',{ error:error.message });
                     } else {             
                         res.redirect('/usuaris');
                     }
                 });
             } else {
                 var list_carrecs = Usuari.schema.path('carrec').enumValues;
-                res.render('usuaris/new', {carrecs:list_carrecs, error: "Usuari ja registrat"});
+                res.render('usuaris/new', { carrecs:list_carrecs, error: "Usuari ja registrat" });
+            }
+        });
+    }
+
+    static update_get(req, res, next) {
+        Usuari.findById(req.params.id, function (err, usuari) {
+            if (err) {
+                return next(err);
+            }
+            if (usuari == null) {
+              // No results.
+              var err = new Error("Usuari not found");
+              err.status = 404;
+              return next(err);
+            }
+            // Success.
+            var list_carrecs = Usuari.schema.path('carrec').enumValues;
+            res.render("usuaris/update", { usuari: usuari, carrecs: list_carrecs });
+        });
+    }
+
+    static async update_post(req, res, next) {
+        // Encriptacio de password
+        let password = req.body.password;
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(password, salt);
+
+        var usuari = new Usuari({
+            _id: req.params.id,  // Necessari per a que sobreescrigui el mateix objecte!
+            nom: req.body.nom,
+            cognoms: req.body.cognoms,
+            dni: req.body.dni,
+            carrec: req.body.carrec,
+            email: req.body.email,
+            password: req.body.password,
+        });
+      
+        Usuari.findByIdAndUpdate(
+            req.params.id,
+            usuari,
+            {runValidators: true}, // comportament per defecte: buscar i modificar si el troba sense validar l'Schema
+            function (err, usuariFound) {
+                let list_carrecs = Usuari.schema.path('carrec').enumValues;
+                if (err) {
+                    res.render("usuaris/update", { usuari: usuari, error: err.message, carrecs: list_carrecs });
+                }          
+                res.render("usuaris/update", { usuari: usuari, message: 'User Updated', carrecs: list_carrecs});
+            }
+        );
+    }
+
+    static async delete_get(req, res, next) {
+        res.render('usuaris/delete', { id: req.params.id });
+    }
+
+    static async delete_post(req, res, next) {
+        Usuari.findByIdAndRemove(req.params.id, function (error) {
+            if(error){
+                res.redirect('/usuaris');
+            }else{
+                res.redirect('/usuaris');
             }
         });
     }
--- routes/usuarisRouter.js
+++ routes/usuarisRouter.js
@@ -8,4 +8,10 @@ router.get("/", usuaris_controller.list);
 router.get("/create", usuaris_controller.create_get);
 router.post("/create", usuaris_controller.create_post);
 
+router.get("/update/:id", usuaris_controller.update_get);
+router.post("/update/:id", usuaris_controller.update_post);
+
+router.get("/delete/:id", usuaris_controller.delete_get);
+router.post("/delete/:id", usuaris_controller.delete_post);
+
 module.exports = router;
