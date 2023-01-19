3e8a800964208ea3fb5c58b2765205395a62dca9
jordi <jordi.mallafre@hotmail.com>
2023-01-19 08:21:58 +0100

    Creació dels models comentari i tramet

---
+++ models/tramet.js
@@ -0,0 +1,12 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var TrametSchema = new Schema({
+    codiIncidencia: [{ type: Schema.ObjectId, ref: "Incidencia" }],
+    codiUsuari: [{ type: Schema.ObjectId, ref: "Usuari" }],
+    codiComentari: [{ type: Schema.ObjectId, ref: "Tramet" }]
+});
+
+// Export model.
+module.exports = mongoose.model("Tramet", TrametSchema);
