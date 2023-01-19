6014b2b6111784841c45231dc17db9f096b4a5d6
jordi <jordi.mallafre@hotmail.com>
2023-01-19 10:31:45 +0100

    Metodes list i create de comentaris

--- routes/comentariRoute.js
+++ routes/comentariRoute.js
@@ -3,12 +3,10 @@ var router = express.Router();
 
 const comentari_controller = require("../controllers/comentariController");
 
+router.get("/list/:id", comentari_controller.list);
 
-
-router.get("/:id", comentari_controller.list);
-
-// router.get("/create", comentari_controller.create_get);
-// router.post("/create", comentari_controller.create_post);
+router.get("/create", comentari_controller.create_get);
+router.post("/create", comentari_controller.create_post);
 
 // router.get("/delete/:id",  comentari_controller.delete_get);
 // router.post("/delete/:id",  comentari_controller.delete_post);
