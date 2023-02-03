34665cbda89ae08f1f89d53755f6dc17f84f0398
Francesc Navarro <cesccat82@gmail.com>
2023-02-03 09:15:37 +0100

    imatges materials

--- routes/prestecRouter.js
+++ routes/prestecRouter.js
@@ -3,8 +3,6 @@ var router = express.Router();
 
 const prestec_controller = require("../controllers/prestecController");
 
-
-
 router.get("/", prestec_controller.list);
 
 router.get("/create", prestec_controller.create_get);
@@ -15,8 +13,5 @@ router.post("/delete/:id", prestec_controller.delete_post);
     
 router.get("/update/:id", prestec_controller.update_get);
 router.post("/update/:id", prestec_controller.update_post);
-
-
-
 
 module.exports=router;
